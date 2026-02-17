25721a34ccadbaf2a99301076a3c8f71f0c53a2f
Tyler Fibonacci <gepheum@gmail.com>
2026-02-17 07:33:04 -0500

    s/skir studio/rpc studio

--- client/skir.cc
+++ client/skir.cc
@@ -1024,7 +1024,8 @@ std::string GetStudioHtml(absl::string_view studio_app_js_url) {
 <html>
   <head>
     <meta charset="utf-8" />
-    <title>Skir Studio</title>
+    <title>RPC Studio</title>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>">
     <script src=")html",
                       escaped_url,
                       R"html("></script>
